bb790a1d7475a45460a5a7788b2f0797514a89ad
tp6910918 <40918139@gcloud.csu.edu.tw>
2023-01-09 11:26:16 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-18=w9az+vrgj*ssr*mm5w(p#zh5)5%(%y)65_xmwelba1u33(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
